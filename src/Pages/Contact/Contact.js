eacfb1a0593a7ef166f2be245f53c54ef4b9ceee
Debabrata Saha <sahadebabrata570@gmail.com>
2021-11-07 00:07:29 +0600

    update

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import './Contact.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee, faPhoneVolume, faEnvelope, faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons'
+
 const Contact = () => {
     return (
         <>
@@ -8,31 +11,57 @@ const Contact = () => {
                     <div className="col-md-6 eye-pearl-heading">
                         <h1><span>We Love</span> To Hear From </h1>
                         <h1>Our Customers</h1>
-                        <div>
+                        <div className="d-flex align-items-center ">
+                            <h3><FontAwesomeIcon className="eye-pearl-contact-icon" icon={faPhoneVolume} /></h3>
+                            <div className="m-3">
+                                <h5>222 333 888</h5>
+                                <h5>12 332 8384 000</h5>
+                            </div>
 
                         </div>
+                        <hr className="w-75" />
+                        <div className="d-flex align-items-center">
+                            <h3><FontAwesomeIcon className="eye-pearl-contact-icon" icon={faEnvelope} /></h3>
+                            <div className="m-3">
+                                <h5>abc@company.com</h5>
+                                <h5>info@company.com</h5>
+                            </div>
+
+                        </div>
+                        <hr className="w-75" />
+                        <div className="d-flex align-items-center">
+                            <h3><FontAwesomeIcon className="eye-pearl-contact-icon" icon={faMapMarkerAlt} /></h3>
+                            <div className="m-3">
+                                <h5>4384 Desert Broom Court</h5>
+                                <h5>Montreal , Canada</h5>
+                            </div>
+
+                        </div>
+                        <hr className="w-75" />
+
+
                     </div>
 
                     <div className="col-md-6 eye-pearl-input-parent">
 
                         <div className="row ">
                             <div className="col-md-5">
-                                <input type="text" className="form-control p-3" placeholder="Your Name" />
+                                <input type="text" className="form-control p-4" placeholder="Your Name" />
                             </div>
                             <div className="col-md-5">
-                                <input type="text" className="form-control p-3" placeholder="Email Address" />
+                                <input type="text" className="form-control p-4" placeholder="Email Address" />
                             </div>
                         </div>
-                        <div className="row mt-3">
+                        <div className="row mt-4">
                             <div className="col-md-5">
-                                <input type="text" className="form-control p-3" placeholder="Phone Number" />
+                                <input type="text" className="form-control p-4" placeholder="Phone Number" />
                             </div>
                             <div className="col-md-5">
-                                <input type="text" className="form-control p-3" placeholder="Subject" />
+                                <input type="text" className="form-control p-4" placeholder="Subject" />
                             </div>
                         </div>
 
-                        <textarea className="form-control my-3 " placeholder="write messages" ></textarea>
+                        <textarea className="form-control mb-3 mt-4" placeholder="write messages" ></textarea>
 
                         <button className="EP-Footer-btn rounded-pill py-3 px-4">Send Message</button>
                     </div>
