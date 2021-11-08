3314d8d35f56c2b0acf2e5b455e86b266d118c57
Debabrata Saha <sahadebabrata570@gmail.com>
2021-11-08 15:06:31 +0600

    update

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -8,7 +8,7 @@ const Contact = () => {
         <>
             <section className="container my-5 ">
                 <div className="row">
-                    <div className="col-md-6 eye-pearl-heading">
+                    <div className="col-md-6 col-11 eye-pearl-heading">
                         <h1><span>We Love</span> To Hear From </h1>
                         <h1>Our Customers</h1>
                         <div className="d-flex align-items-center ">
@@ -42,26 +42,26 @@ const Contact = () => {
 
                     </div>
 
-                    <div className="col-md-6 eye-pearl-input-parent">
+                    <div className="col-md-6  col-12 eye-pearl-input-parent">
 
                         <div className="row ">
-                            <div className="col-md-5">
+                            <div className="col-md-5 col-10">
                                 <input type="text" className="form-control p-4" placeholder="Your Name" />
                             </div>
-                            <div className="col-md-5">
+                            <div className="col-md-5 col-10 my-2">
                                 <input type="text" className="form-control p-4" placeholder="Email Address" />
                             </div>
                         </div>
                         <div className="row mt-4">
-                            <div className="col-md-5">
+                            <div className="col-md-5 col-10">
                                 <input type="text" className="form-control p-4" placeholder="Phone Number" />
                             </div>
-                            <div className="col-md-5">
+                            <div className="col-md-5 col-10 my-2">
                                 <input type="text" className="form-control p-4" placeholder="Subject" />
                             </div>
                         </div>
 
-                        <textarea className="form-control mb-3 mt-4" placeholder="write messages" ></textarea>
+                        <textarea className="form-control  mb-3 mt-4" placeholder="write messages" ></textarea>
 
                         <button className="EP-Footer-btn rounded-pill py-3 px-4">Send Message</button>
                     </div>
