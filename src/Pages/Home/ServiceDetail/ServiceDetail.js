f8aac868c68cae48f5e091947f94e57e00d74adb
Debabrata Saha <sahadebabrata570@gmail.com>
2021-11-08 23:15:30 +0600

    udate

--- src/Pages/Home/ServiceDetail/ServiceDetail.js
+++ src/Pages/Home/ServiceDetail/ServiceDetail.js
@@ -8,7 +8,6 @@ const ServiceDetail = () => {
 
 
     useEffect(() => {
-        // fetch('https://raw.githubusercontent.com/Programming-Hero-Web-Course3/healthcare-related-website-DebabrataSaha-570/main/public/serviceDetail.json?token=ASJSFLUAXASRDOBW6N7NVLLBRAN6G')
         fetch('./serviceDetail.json')
             .then(res => res.json())
             .then(data => {
@@ -16,12 +15,7 @@ const ServiceDetail = () => {
                 console.log(data)
             })
     }, [])
-    // const found = services.filter(product => product.id === id)
-    // console.log(found)
 
-    for (const product of services) {
-        console.log(product)
-    }
     return (
         <div className="container my-5">
             <h3>This is service detail component : {id} {services.length} </h3>
